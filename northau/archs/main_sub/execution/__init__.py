1feb9ef2cec1e29a2fb397e1f7496d6710562f24
Zhenhua Han <hzhua201@gmail.com>
2025-09-07 21:01:57 +0800

    Refactor Executor  (#23)
    * mv execution loop to executor.py
    
    * fix AfterModelHook is not triggered when no tool call

--- northau/archs/main_sub/execution/__init__.py
+++ northau/archs/main_sub/execution/__init__.py
@@ -2,14 +2,14 @@
 
 from .tool_executor import ToolExecutor
 from .subagent_manager import SubAgentManager
-from .response_generator import ResponseGenerator
+from .llm_caller import LLMCaller
 from .executor import Executor
 from .batch_processor import BatchProcessor
 
 __all__ = [
     'ToolExecutor',
     'SubAgentManager', 
-    'ResponseGenerator',
+    'LLMCaller',
     'Executor',
     'BatchProcessor'
 ]
