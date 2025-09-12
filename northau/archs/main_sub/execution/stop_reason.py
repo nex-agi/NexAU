0028940ccb8bfb45f176cee16aa9bc8ca6215a0c
Kai Hu <kaihu.kh@gmail.com>
2025-09-12 16:10:07 +0800

    add agent stop reason (#31)

---
+++ northau/archs/main_sub/execution/stop_reason.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+
+
+class AgentStopReason(Enum):
+    """Enumerates reasons why agent execution may stop."""
+    MAX_ITERATIONS_REACHED = auto()
+    STOP_TOOL_TRIGGERED = auto()
+    ERROR_OCCURRED = auto()
+    CONTEXT_TOKEN_LIMIT = auto()
+    SUCCESS = auto()
+    NO_MORE_TOOL_CALLS = auto()
+
