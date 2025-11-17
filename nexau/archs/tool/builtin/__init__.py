1ad1205cece284566c64df2dc5bd40ba5160f272
Zhenhua Han <hzhua201@gmail.com>
2025-11-17 14:28:46 +0800

    Add license header to all py, sh, js files (#83)

--- nexau/archs/tool/builtin/__init__.py
+++ nexau/archs/tool/builtin/__init__.py
@@ -1,3 +1,17 @@
+# Copyright (c) Nex-AGI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .bash_tool import bash_tool
 from .file_tools.file_edit_tool import file_edit_tool
 from .file_tools.file_read_tool import file_read_tool
